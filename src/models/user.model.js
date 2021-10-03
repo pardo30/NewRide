9ce64186feafc894e678aedafc0103cac53eeec7
pardo30 <info@pardo30.com>
2021-10-03 11:50:59 +0200

    User Model + Bcrypt

---
+++ src/models/user.model.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+})
+
+userSchema.methods.verifyPassword = function(password) {
+    return bcrypt.compareSync(password, this.password)
+}
+
+userSchema.methods.encryptPassword = async password => {
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hashSync(password, salt)
+}
+
+module.exports = model("User", userSchema);
