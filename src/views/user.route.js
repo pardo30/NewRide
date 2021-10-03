eff91cccf380bc3395e2bbd6d757255c96cd9932
pardo30 <info@pardo30.com>
2021-10-03 11:35:24 +0200

    Initial user router structure

--- src/views/user.route.js
+++ src/views/user.route.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router
+    .post('/register', register)
+    .post('/login', login)
+    .get('/authenticate', authenticate)
+    .get('/showAllUser', showAll)
+
 module.exports = router;
